79cd5677975b7fd95041aa4ea6721ca9b910a5ca
Sharon Kirimi <sharonkirimi@Sharons-MacBook-Pro.local>
2021-10-23 23:25:06 +0200

    Corrections to the previous commit

--- app/games/events.js
+++ app/games/events.js
@@ -1,103 +1,19 @@
-const spaces = ["", "", "", "", "", "", "", "", ""];
+const onStartGame = function (event) {
+  event.preventDefault();
+  const gameStart = event.target;
 
-let count = 0;
-const box = [];
-let i = 0;
-
-$(".row div").each(function () {
-  box[i++] = $(this).attr("id");
-});
-
-const winningOptions = [
-  [box[0], box[1], box[2]],
-  [box[3], box[4], box[5]],
-  [box[6], box[7], box[8]],
-  [box[0], box[3], box[6]],
-  [box[1], box[4], box[7]],
-  [box[2], box[5], box[8]],
-  [box[0], box[4], box[8]],
-  [box[2], box[4], box[6]],
-];
-
-const selectBox = function (event) {
-  const id = event.target.id;
-  if (!spaces[id]) {
-    if (count % 2 === 0) {
-      currentPlayer = $("#" + id).text(player1);
-      $("#" + id).off("click");
-      spaces[id] = document.getElementById(id);
-    } else {
-      currentPlayer = $("#" + id).text(player2);
-      $("#" + id).off("click");
-      spaces[id] = document.getElementById(id);
-    }
-    count++;
-  }
-
-  winningPlayer();
-
-  console.log(spaces);
-
-  return currentPlayer;
-};
-const winningPlayer = function (event) {
-  for (let idx = 0; idx < 8; idx++) {
-    if (
-      spaces[winningOptions[idx][0]].innerHTML ===
-        spaces[winningOptions[idx][1]].innerHTML &&
-      spaces[winningOptions[idx][1]].innerHTML ===
-        spaces[winningOptions[idx][2]].innerHTML &&
-      spaces[winningOptions[idx][2]].innerHTML ===
-        spaces[winningOptions[idx][0]].innerHTML &&
-      spaces[winningOptions[idx][1]].innerHTML !== "" &&
-      spaces[winningOptions[idx][1]] !== ""
-    ) {
-      console.log(spaces[winningOptions[idx][0]].innerHTML);
-      $("#results")
-        .text(`${spaces[winningOptions[idx][0]].innerHTML} is the WINNER`)
-        .addClass("gradient-text");
-
-      $("#tic-tac-toe div").off("click", selectBox);
-    } else if (count === 9) {
-      $("#results").text("Tie Game").removeClass("gradient-text");
-    }
-  }
+  api.startGame(gameStart).then(ui.startGameSuccess).catch(ui.startGameFailure);
 };
 
-const startGameSuccess = function (responseData) {
-  $("#user-display").text("Game successfully started");
-  $("#user-display").removeClass();
-  $("#user-display").addClass("text-white");
+const onResetGame = function (event) {
+  event.preventDefault();
 
-  $("#tic-tac-toe div").on("click", selectBox);
+  const reset = event;
 
-  setTimeout(() => $("#user-display").text(""), 3000);
-};
-
-const startGameFailure = function () {
-  $("#error-message").text(
-    "There was a error starting the game. Please try again"
-  );
-
-  $("#error-message").removeClass();
-  $("#error-message").addClass("text-light");
-
-  setTimeout(() => $("#error-message").text(""), 3000);
-};
-
-const resetGame = () => {
-  $(".box").text("");
-  $("#results").text("");
-  for (let i = 0; i < spaces.length; i++) {
-    spaces[i] = "";
-  }
-  count = 0;
+  ui.resetGame(reset);
 };
 
 module.exports = {
-  startGameSuccess,
-  startGameFailure,
-  selectBox,
-  winningPlayer,
-  resetGame,
+  onStartGame,
+  onResetGame,
 };
--- app/games/ui.js
+++ app/games/ui.js
@@ -1,120 +1,103 @@
-const onCreateSuccess = function (responseData) {
-  $("#games-display").text("Game successfully created");
+const spaces = ["", "", "", "", "", "", "", "", ""];
 
-  $("#games-display").removeClass();
-  $("#games-display").addClass("text-success");
+let count = 0;
+const box = [];
+let i = 0;
 
-  // extract the move from the response's data
-  // update the div with the id `games-display` to have html for our game
-  const game = responseData.game;
-  $("#games-display").html(`
-    <div>
-      <h3>${game.title}</h3>
-      <p>Directed by: ${game.director}</p>
-      <p>ID: ${game._id}</p>
-    </div>
-  `);
+$(".row div").each(function () {
+  box[i++] = $(this).attr("id");
+});
 
-  $("form").trigger("reset");
-  console.log("onCreateSuccess ran. responseData is :", responseData);
+const winningOptions = [
+  [box[0], box[1], box[2]],
+  [box[3], box[4], box[5]],
+  [box[6], box[7], box[8]],
+  [box[0], box[3], box[6]],
+  [box[1], box[4], box[7]],
+  [box[2], box[5], box[8]],
+  [box[0], box[4], box[8]],
+  [box[2], box[4], box[6]],
+];
+
+const selectBox = function (event) {
+  const id = event.target.id;
+  if (!spaces[id]) {
+    if (count % 2 === 0) {
+      currentPlayer = $("#" + id).text(player1);
+      $("#" + id).off("click");
+      spaces[id] = document.getElementById(id);
+    } else {
+      currentPlayer = $("#" + id).text(player2);
+      $("#" + id).off("click");
+      spaces[id] = document.getElementById(id);
+    }
+    count++;
+  }
+
+  winningPlayer();
+
+  console.log(spaces);
+
+  return currentPlayer;
+};
+const winningPlayer = function (event) {
+  for (let idx = 0; idx < 8; idx++) {
+    if (
+      spaces[winningOptions[idx][0]].innerHTML ===
+        spaces[winningOptions[idx][1]].innerHTML &&
+      spaces[winningOptions[idx][1]].innerHTML ===
+        spaces[winningOptions[idx][2]].innerHTML &&
+      spaces[winningOptions[idx][2]].innerHTML ===
+        spaces[winningOptions[idx][0]].innerHTML &&
+      spaces[winningOptions[idx][1]].innerHTML !== "" &&
+      spaces[winningOptions[idx][1]] !== ""
+    ) {
+      console.log(spaces[winningOptions[idx][0]].innerHTML);
+      $("#results")
+        .text(`${spaces[winningOptions[idx][0]].innerHTML} is the WINNER`)
+        .addClass("gradient-text");
+
+      $("#tic-tac-toe div").off("click", selectBox);
+    } else if (count === 9) {
+      $("#results").text("Tie Game").removeClass("gradient-text");
+    }
+  }
 };
 
-const onCreateFailure = function (error) {
-  $("#error-message").text("Error on creating game");
+const startGameSuccess = function (responseData) {
+  $("#user-display").text("Game successfully started");
+  $("#user-display").removeClass();
+  $("#user-display").addClass("text-white");
+
+  $("#tic-tac-toe div").on("click", selectBox);
+
+  setTimeout(() => $("#user-display").text(""), 3000);
+};
+
+const startGameFailure = function () {
+  $("#error-message").text(
+    "There was a error starting the game. Please try again"
+  );
+
   $("#error-message").removeClass();
-  $("#error-message").addClass("text-danger");
-  console.error("onCreateFailure ran. Error is :", error);
+  $("#error-message").addClass("text-light");
+
+  setTimeout(() => $("#error-message").text(""), 3000);
 };
 
-const onIndexSuccess = function (responseData) {
-  $("#games-display").text("All Games successfully received");
-  $("#games-display").removeClass();
-  console.log("onIndexSuccess ran. responseData is :", responseData.games);
-
-  let gamesHtml = "";
-  const games = responseData.games;
-  games.forEach((game) => {
-    gamesHtml += `
-    <div>
-      <h3>${game.title}</h3>
-      <p>Directed by: ${game.director}</p>
-      <p>ID: ${game._id}</p>
-    </div>
-  `;
-  });
-  $("#games-display").html(gamesHtml);
-};
-
-const onIndexFailure = function (error) {
-  $("#error-message").text("Error on getting games");
-  $("#error-message").removeClass();
-  $("#error-message").addClass("text-danger");
-  console.error("onIndexFailure ran. Error is :", error);
-};
-
-const onShowSuccess = function (responseData) {
-  $("#games-display").text("One Game successfully received");
-  $("#games-display").removeClass();
-  $("#games-display").addClass("text-success");
-  console.log("onShowSuccess ran. responseData is :", responseData);
-  $("form").trigger("reset");
-
-  const game = responseData.game;
-  $("#games-display").html(`
-    <div>
-      <h3>${game.title}</h3>
-      <p>Directed by: ${game.director}</p>
-      <p>ID: ${game._id}</p>
-    </div>
-  `);
-};
-
-const onShowFailure = function (error) {
-  $("#error-message").text("Error on getting game");
-  $("#error-message").removeClass();
-  $("#error-message").addClass("text-danger");
-  console.error("onShowFailure ran. Error is :", error);
-};
-
-const onDestroySuccess = function () {
-  $("#games-display").text("Game successfully deleted");
-  $("#games-display").removeClass();
-  $("#games-display").addClass("text-success");
-  $("form").trigger("reset");
-  console.log("Game successfully deleted");
-};
-
-const onDestroyFailure = function (error) {
-  $("#error-message").text("Error on deleting game");
-  $("#error-message").removeClass();
-  $("#error-message").addClass("text-danger");
-  console.error("onDestroyFailure ran. Error is :", error);
-};
-
-const onUpdateSuccess = function () {
-  $("#games-display").text("Game successfully updated");
-  $("#games-display").removeClass();
-  $("#games-display").addClass("text-success");
-  $("form").trigger("reset");
-  console.log("Game successfully updated");
-};
-
-const onUpdateFailure = function (error) {
-  $("#error-message").text("Error on updating game");
-  $("#error-message").removeClass();
-  $("#error-message").addClass("text-danger");
-  console.error("onUpdateFailure ran. Error is :", error);
+const resetGame = () => {
+  $(".box").text("");
+  $("#results").text("");
+  for (let i = 0; i < spaces.length; i++) {
+    spaces[i] = "";
+  }
+  count = 0;
 };
 
 module.exports = {
-  onCreateSuccess,
-  onCreateFailure,
-  onIndexSuccess,
-  onIndexFailure,
-  onShowSuccess,
-  onShowFailure,
-  onDestroySuccess,
-  onDestroyFailure,
-  onUpdateSuccess,
-  onUpdateFailure,
+  startGameSuccess,
+  startGameFailure,
+  selectBox,
+  winningPlayer,
+  resetGame,
 };
